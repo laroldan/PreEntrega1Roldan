d8ce4a7135b0227e2b0ca3ed42ba17ad389624c9
Lautaro Roldan <lautaro-22@live.com.ar>
2023-03-18 16:07:07 -0300

    start js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//MENU DESPLEGABLE DE GESTION
+
+alert("Hola bienvenido al gestor de opciones de TECHCOM");
+
+let opcion = prompt(
+  "Escriba la opcion que desea acceder:\n1-Saldo en cuenta\n2-Extracción de dinero\n3-Cargar dinero\n4-Detalles de cuenta\n5-Ajustes de cuenta\n6-Salir"
+);
