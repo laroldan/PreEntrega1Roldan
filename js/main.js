031bf60c77e1789a01850bce2e38a03f002fa7f8
Lautaro Roldan <lautaro-22@live.com.ar>
2023-03-19 21:42:05 -0300

    end of js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //MENU DESPLEGABLE DE GESTION
 
-alert("Hola bienvenido al CALCULADOR:COM");
+alert("Hola bienvenido al CALCULADORcom");
 
 let nombre = prompt("Ingrese su nombre:");
 console.log(nombre);
@@ -13,57 +13,68 @@ console.log("Numero 2: " + num2);
 let opcion;
 let aux;
 
-function suma(n1,n2) {
-  this.primero = n1;
-  this.segundo = n2;
+function mensaje() {
+  alert("Gracias por utilizar la calculadora " + nombre);
+  console.log(
+    "Gracias por utilizar la calculadora " +
+      nombre +
+      "\nF5 para volver a iterar."
+  );
+}
+
+function suma(n1, n2) {
   let resu = n1 + n2;
   console.log("RESULTADO DE LA SUMA ES= " + resu);
-  alert("SUMA REALIZADA EXITOSAMENTE\n\n En la consola aparecen los resultados, gracias.");
+  alert(
+    "SUMA REALIZADA EXITOSAMENTE\n\n En la consola aparecen los resultados, gracias."
+  );
 }
 
-function resta(n1,n2) {
-  this.primero = n1;
-  this.segundo = n2;
+function resta(n1, n2) {
   let resu = n1 - n2;
   console.log("RESULTADO DE LA RESTA ES= " + resu);
-  alert("RESTA REALIZADA EXITOSAMENTE\n\n En la consola aparecen los resultados, gracias.");
+  alert(
+    "RESTA REALIZADA EXITOSAMENTE\n\n En la consola aparecen los resultados, gracias."
+  );
 }
 
-function mult(n1,n2) {
-  this.primero = n1;
-  this.segundo = n2;
+function mult(n1, n2) {
   let resu = n1 * n2;
   console.log("RESULTADO DE LA MULTIPLICACION ES= " + resu);
-  alert("MULTIPLICACION REALIZADA EXITOSAMENTE\n\n En la consola aparecen los resultados, gracias.");
+  alert(
+    "MULTIPLICACION REALIZADA EXITOSAMENTE\n\n En la consola aparecen los resultados, gracias."
+  );
 }
 
-function divi(n1,n2) {
-  this.primero = n1;
-  this.segundo = n2;
+function divi(n1, n2) {
   let resu = n1 / n2;
   console.log("RESULTADO DE LA DIVISION ES= " + resu);
-  alert("DIVISION REALIZADA EXITOSAMENTE\n\n En la consola aparecen los resultados, gracias.");
+  alert(
+    "DIVISION REALIZADA EXITOSAMENTE\n\n En la consola aparecen los resultados, gracias."
+  );
 }
 
-do{
-opcion = parseInt(prompt("MENU DE CALCULADORA\n\n1-Sumar\n2-Restar\n3-Multiplicar\n4-Dividir\n5-Cambiar numero\n6-Salir\nEliga la opcion que desee:"));
-
-}while(opcion <1 || opcion >6);
+do {
+  opcion = parseInt(
+    prompt(
+      "MENU DE CALCULADORA\n\n1-Sumar\n2-Restar\n3-Multiplicar\n4-Dividir\n5-Cambiar numero\n6-Salir\nEliga la opcion que desee:"
+    )
+  );
+} while (opcion < 1 || opcion > 6);
 
 while (opcion != 6) {
-  
   switch (opcion) {
     case 1:
-        suma(num1,num2);
+      suma(num1, num2);
       break;
     case 2:
-        resta(num1,num2);
+      resta(num1, num2);
       break;
     case 3:
-        mult(num1,num2);
+      mult(num1, num2);
       break;
     case 4:
-        divi(num1,num2);
+      divi(num1, num2);
       break;
     case 5:
       num1 = parseFloat(prompt("Ingrese el primer numero:"));
@@ -76,10 +87,13 @@ while (opcion != 6) {
       break;
   }
 
-  do{
-    opcion = parseInt(prompt("MENU DE CALCULADORA\n\n1-Sumar\n2-Restar\n3-Multiplicar\n4-Dividir\n5-Cambiar numero\n6-Salir\nEliga la opcion que desee:"));
-    }while(opcion <1 || opcion >6);
-
+  do {
+    opcion = parseInt(
+      prompt(
+        "MENU DE CALCULADORA\n\n1-Sumar\n2-Restar\n3-Multiplicar\n4-Dividir\n5-Cambiar numero\n6-Salir\nEliga la opcion que desee:"
+      )
+    );
+  } while (opcion < 1 || opcion > 6);
 }
 
-mensaje
+mensaje();
